0a6cfbb1b125a4023f1b19584b53d16dcbb761fd
Alexa Aguilera <43582999+alexaaguilera@users.noreply.github.com>
2019-11-10 00:55:30 -0600

    Update navbar-index.component.ts
    nav bar details

--- src/app/navbar-index/navbar-index.component.ts
+++ src/app/navbar-index/navbar-index.component.ts
@@ -1,8 +1,6 @@
-
-import { Component , OnInit } from '@angular/core';
-import { AuthService } from './../auth.service';
-import { Router } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./../auth.service";
+import { Router } from "@angular/router";
 
 /*  Aquí vamos a poner el tipo de navbar que se va a desplegar
       1: Index, que es el principal
@@ -10,39 +8,31 @@ import { Router } from '@angular/router';
       3: Paciente
 */
 
- 
-
-
 @Component({
-  selector: 'app-navbar-index',
-  templateUrl: './navbar-index.component.html',
-  styleUrls: ['./navbar-index.component.scss']
+  selector: "app-navbar-index",
+  templateUrl: "./navbar-index.component.html",
+  styleUrls: ["./navbar-index.component.scss"]
 })
-
-
-export class NavbarIndexComponent implements OnInit{
-
+export class NavbarIndexComponent implements OnInit {
   //Esta variable va a guardar si ya se iniciaron sesión
   isLoggedin;
   loginType;
-  
-  constructor(private router: Router, private auth: AuthService) { }
 
-  
-  
+  constructor(private router: Router, private auth: AuthService) {}
 
   ngOnInit() {
-
     //Este evento se suscribe al login
     this.router.events.subscribe(event => {
       if (event.constructor.name === "NavigationEnd") {
-       this.isLoggedin = !this.auth.isLoggedIn;
-       this.loginType = this.auth.loginType;
-       
+        let _auth = false;
+        if (localStorage.getItem("auth")) {
+          _auth = localStorage.getItem("auth") == "1" ? true : false;
+        }
+
+        // this.isLoggedin = !this.auth.isLoggedIn;
+        this.isLoggedin = !_auth;
+        this.loginType = localStorage.getItem("type") || ""; //this.auth.loginType;
       }
-    })
-    
+    });
   }
-
-
 }
