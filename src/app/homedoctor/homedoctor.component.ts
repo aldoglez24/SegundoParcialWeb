811ed1636f710cdaf1db3e425682b50e03196cd0
Aldo <a01332737@itesm.mx>
2019-11-12 13:11:58 -0600

    doctor component

--- src/app/homedoctor/homedoctor.component.ts
+++ src/app/homedoctor/homedoctor.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 
+
+
+
+import * as $ from 'jquery';
+
+
 @Component({
   selector: "app-homedoctor",
   templateUrl: "./homedoctor.component.html",
@@ -10,7 +16,18 @@ import { Observable } from "rxjs/Observable";
 export class HomedoctorComponent implements OnInit {
   insights: any;
 
-  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    $(document).ready(function () {
+      $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("menuDisplayed");
+      });
+    });
+
+
+  }
 }
