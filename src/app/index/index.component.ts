2d4f011c590506840984611fc078bc3eae97715f
Aldo <a01332737@itesm.mx>
2019-11-12 17:59:44 -0600

    pie chart

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -45,21 +45,6 @@ export class IndexComponent implements OnInit {
     }
   };
 
-
-  /*
-    public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
-    public pieChartData: number[] = [300, 500, 100];
-    public pieChartType: ChartType = 'pie';
-    public pieChartLegend = true;
-    public pieChartPlugins = [pluginDataLabels];
-    public pieChartColors = [
-      {
-        backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
-      },
-    ];
-  */
-
-
   public pieChartLabels: Label[] = [];
   public pieChartData: number[] = [];
   public pieChartType: ChartType = 'pie';
